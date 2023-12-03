fb755c5db8bef92654bccd6241bc98b016725460
trippyx <basican.kuldeep@gmail.com>
2023-12-03 19:03:55 +0530

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Kuldeep Singh on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
